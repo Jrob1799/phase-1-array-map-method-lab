cd507b2d4ffb348a922eb2100cd761363311e293
Jacob <jacobwroberts33@gmail.com>
2023-03-30 16:08:24 -0400

    completed

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => {
+    const words = title.split(' ');
+    const capitalizedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return capitalizedWords.join(' ');
+  });
 }
